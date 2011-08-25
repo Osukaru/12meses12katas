54bf4071cca00ac742936db142e2f4fa02ccea74
Osukaru <zepolracso@gmail.com>
2011-08-25 14:42:42 +0200

    Una cadena con un valor me devolverá el propio valor

--- Enero-String-Calculator/javascript/spec/StringCalculatorSpec.js
+++ Enero-String-Calculator/javascript/spec/StringCalculatorSpec.js
@@ -2,5 +2,19 @@ describe("StringCalculator", function() {
 	it("Una cadena vacia me devolvera como resultado el valor 0", function() {
 		expect(StringCalculator.add("")).toEqual(0);
 	});
-});
+	
+	describe("Una cadena con un valor me devolverá el propio valor:", function() {
+		it("--> se cumple con el valor 0", function() {
+			expect(StringCalculator.add("0")).toEqual(0);
+		});
 
+		it("--> se cumple con el valor 1", function() {
+			expect(StringCalculator.add("1")).toEqual(1);
+		});	
+		
+		it("--> se cumple con el valor 2", function() {
+			expect(StringCalculator.add("2")).toEqual(2);
+		});
+
+	});
+});
--- Enero-String-Calculator/javascript/src/StringCalculator.js
+++ Enero-String-Calculator/javascript/src/StringCalculator.js
@@ -1,6 +1,12 @@
 var StringCalculator = function() {
-	var sumaLosNumerosDeLaCadena = function( cadenaConNumeros ) {  	   
-		return 0;
+	var BASE_DECIMAL = 10,
+		sumaLosNumerosDeLaCadena = function( cadenaConNumeros ) {  	   
+		if (cadenaConNumeros === "") {
+			return 0;
+		}
+		else {
+			return parseInt(cadenaConNumeros, BASE_DECIMAL);
+		}
 	}	   
 	return {
 		add : sumaLosNumerosDeLaCadena
